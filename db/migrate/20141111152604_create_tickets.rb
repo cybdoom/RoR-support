2f95099ca11be22aaf48e2271c0a2cdc1f259a9e
cybdoom <cybdoom@gmail.com>
2014-11-11 17:39:02 +0200

    Added migration for creating tickets table

---
+++ db/migrate/20141111152604_create_tickets.rb
@@ -0,0 +1,23 @@
+class CreateTickets < ActiveRecord::Migration
+  def change
+    create_table :tickets do |t|
+      t.belongs_to :user
+
+      t.string :customer_name
+      t.string :customer_email
+
+      t.integer :department
+      t.string :subject
+      t.string :description
+
+      t.integer :status
+
+      t.timestamps
+    end
+
+    add_index :tickets, :user_id, unique: false
+    add_index :tickets, :subject, unique: false
+    add_index :tickets, :department, unique: false
+    add_index :tickets, :status, unique: false
+  end
+end
