b7fbfdca5dee3db9681f26d41a3eec1dcd6ac0bb
cybdoom <cybdoom@gmail.com>
2014-11-11 15:06:57 +0200

    Modified welcome_controller tests

--- test/controllers/welcome_controller_spec.rb
+++ test/controllers/welcome_controller_spec.rb
@@ -1,11 +1,17 @@
 class WelcomeControllerTest < ActionController::TestCase
   def setup
     @user = users(:user_1)
-    sign_in @user
   end
 
   test "should redirect unsigned user to ticket creation page" do
+    sign_out @user
     get :index
-    assert_response :redirect
+    assert_redirected_to '/tickets/new'
+  end
+
+  test "should redirect signed user to tickets list page" do
+    sign_in @user
+    get :index
+    assert_redirected_to '/tickets/index'
   end
 end
