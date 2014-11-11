e7f165153a2b0b3544df6aacc1c4692495cab5d0
cybdoom <cybdoom@gmail.com>
2014-11-12 01:26:32 +0200

    Implemented ticket data prefill after redirect

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,22 +4,27 @@ class TicketsController < ActionController::Base
   before_action :authenticate_user!, only: [:index]
 
   def new
-    @ticket = Ticket.new
+    @ticket = Ticket.new ticket_attributes
   end
 
   def index
   end
 
   def create
-    attributes = params.require(:ticket).permit(:customer_name, :customer_email, :department, :subject, :description)
-    @ticket = Ticket.new attributes
+    @ticket = Ticket.new ticket_attributes
     if @ticket.save
       flash[:notice] = 'Ticket was succesfully created'
+      redirect_to :new
     else
       message = @ticket.errors.messages.each.map {|k, v| "#{k.to_s.humanize}: #{v.join(', ')}" }.join('<br>').html_safe
       flash[:error] = message unless @ticket.save
+      redirect_to action: 'new', ticket: ticket_attributes
     end
+  end
 
-    redirect_to :back
+  private
+
+  def ticket_attributes
+    params.require(:ticket).permit(:customer_name, :customer_email, :department, :subject, :description)
   end
 end
