3a5a30a75f95a43846114c4a79964e6ce925937d
cybdoom <cybdoom@gmail.com>
2014-11-12 17:40:01 +0200

    Improved ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   include Tokenable
 
+  belongs_to :user
+
   DEPARTMENTS = ['some department', 'second department', 'other department']
   STATUSES = ['Waiting for Staff Response', 'Waiting for Customer', 'On Hold', 'Cancelled', 'Completed']
 
@@ -10,4 +12,8 @@ class Ticket < ActiveRecord::Base
   validates :subject, presence: true, length: { minimum: 2, maximum: 64 }
   validates :description, presence: true, length: { minimum: 3, maximum: 1024 }
   validates :status, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: STATUSES.size }
+
+  def department_name
+    DEPARTMENTS[self.department]
+  end
 end
