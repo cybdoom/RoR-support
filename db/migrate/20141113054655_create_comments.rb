9ae3837a835e26d782d552d4e206faa926524784
cybdoom <cybdoom@gmail.com>
2014-11-13 14:57:10 +0200

    Modified migration for comments

--- db/migrate/20141113054655_create_comments.rb
+++ db/migrate/20141113054655_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.belongs_to :ticket
 
+      t.string :text
       t.integer :order
       t.reply :boolean
 
