6f82453943217f891be338aac926667770029998
cybdoom <cybdoom@gmail.com>
2014-11-13 07:02:18 +0200

    Implemented ticket moving around panels

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -1,25 +1,38 @@
 $(function() {
   $(".dropdown-menu").on('click', 'li a', function(e){
     var GetUpdateDetails = function(element) {
+      var field = element.attr('class').split('-')[1];
       return {
-        field: $(element).attr('class').split('-')[1],
-        ticketEntry: $(element).parents('.ticket-entry')
+        field: field,
+        ticketEntry: element.parents('.ticket-entry'),
+        oldValue: element.parents('.ticket-entry').find('.ticket-' + field + '-icon').attr('value')
       }
+    }
+
+    var GetPanelForStatus = function(status) {
+      if (status == 0 || status == 1) return $('.tickets-panels .panel-warning');
+      if (status == 2) return $('.tickets-panels .panel-info');
+      return $('.tickets-panels .panel-success');
     }
 
     var OnTicketUpdateSuccess = function(response) {
       updateDetails.ticketEntry.find('.ticket-' + response.field + '-icon')
         .removeClass()
-        .addClass('fa ' + response.iconClass)
+        .addClass('ticket-' + response.field + '-icon fa ' + response.iconClass)
         .attr('title', response.title);
+
+      if (updateDetails.field == 'status' && response.value != updateDetails.oldValue) {
+        var panel = GetPanelForStatus(response.value);
+        updateDetails.ticketEntry.appendTo(panel);
+      }
     }
 
-    var updateDetails = GetUpdateDetails(this);
-    var updatingTicketId = updateDetails.ticketEntry.find('input').val();
-    var data = {
-      field: updateDetails.field,
-      value: $(this).find('i').attr('value')
-    }
+    var updateDetails = GetUpdateDetails($(this)),
+        updatingTicketId = updateDetails.ticketEntry.find('input').val(),
+        data = {
+          field: updateDetails.field,
+          value: $(this).find('i').attr('value')
+        };
 
     $.ajax({
         url: '/tickets/' + updatingTicketId,
